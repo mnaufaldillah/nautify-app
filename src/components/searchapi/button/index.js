cf56c0ae7fd48ecbd96c1a8ec2e5c61882589f46
mnaufaldillah <mnaufaldillah@gmail.com>
2022-04-04 04:13:11 +0700

    mencoba homework3

---
+++ src/components/searchapi/button/index.js
@@ -0,0 +1,9 @@
+const Button = ({leftIcon, children, ...props}) => {
+    return (
+      <button className='btn' {...props}>
+        {leftIcon} {children}
+      </button>
+    );
+  };
+  
+  export default Button;
---
+++ src/components/searchapi/navbar/index.js
@@ -0,0 +1,27 @@
+import Button from '../button';
+import { FaSpotify } from 'react-icons/fa';
+import { redirect } from '../../lib/auth.js';
+import SearchBar from '../searchBar';
+import Profile from "../profile";
+
+const Navbar = ({userData, handleSearch}) => {
+  const isAuth = userData?.access_token !== undefined;
+  return (
+    <div className='navbar'>
+    {!isAuth ? (
+      <>
+        <SearchBar handleSearch={handleSearch}/>
+        <Profile userData={userData}/>
+      </>
+    ) : (
+      <Button
+        onClick={() => redirect()}
+        leftIcon={<FaSpotify/>}
+        > Login
+      </Button>
+  )}
+     </div>
+  );
+};
+
+export default Navbar;
